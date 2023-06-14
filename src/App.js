bfd5dd163297005d702f7c194ed5b4653483b81d
tylerb1 <tberb1290@gmail.com>
2023-06-14 14:26:06 -0500

    added todo list functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import './App.css'
 
 function App() {
+  const [todoItems, setTodoItems] = useState([])
+  const [newTodoItem, setNewTodoItem] = useState('')
+  const [supabaseClient, setSupabaseClient] = useState()
+
+  useEffect(() => {
+    const newClient = createClient(
+      'https://lzpbefehvaczommiiwbb.supabase.co', 
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6cGJlZmVodmFjem9tbWlpd2JiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3NjUwODMsImV4cCI6MjAwMjM0MTA4M30.x1unuW8CPEJYslx1fG9BUx_IC6OOI3GKJxNo9SRSI9E'
+    )
+    setSupabaseClient(newClient)
+    newClient
+      .from('items')
+      .select('*')
+      .then(response => setTodoItems(response.data))
+  }, [])
+
+  const insertRow = async (todoText) => {
+    const { data, error } = await supabaseClient
+      .from('items')
+      .insert({ todo_text: todoText })
+      .select()
+    setTodoItems([...todoItems, data[0]])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type="text"
+        onChange={(changeEvent) => {
+          setNewTodoItem(changeEvent.target.value)
+        }}
+        onKeyDown={(keyEvent) => {
+          if (keyEvent.key === 'Enter') {
+            insertRow(newTodoItem)
+          }
+        }} 
+      />
+      {todoItems.map((item, index) => <p key={index}>{item.todo_text}</p>)}
     </div>
   );
 }
